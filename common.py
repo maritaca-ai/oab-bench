d80a44181a228e4e30c8fd0659c843e49188d927
Rodrigo Nogueira <121117945+rodrigo-f-nogueira@users.noreply.github.com>
2025-06-18 13:07:05 -0300

    Update common.py

--- common.py
+++ common.py
@@ -179,6 +179,8 @@ def run_judge_single(question, answer, judge, ref_answer, multi_turn=False, clie
 
     if model.startswith("gpt-"):
         judgment = chat_completion_openai(model, conv, temperature=0, max_tokens=2048, client=client)
+    elif model.startswith("gemini"):
+        judgment = chat_completion_openai(model, conv, temperature=0, max_tokens=20000, client=client)
     elif any(model.startswith(m) for m in ["o1", "o3"]):
         # With o1 models and newer, developer messages replace the previous system messages.
         conv.messages[0][0] = "developer"
